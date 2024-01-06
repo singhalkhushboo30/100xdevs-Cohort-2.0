710d20fa9a6d034d392aff1559394d2ee2730a13
Khushboo <khushboosinghal9760@gmail.com>
2024-01-06 19:27:50 +0530

    done

---
+++ 0-1_Cohort/Week-6/Class-Content/01app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <React.Fragment>
+    <p>hi</p>
+    </React.Fragment>
+  )
+}
+
+export default App

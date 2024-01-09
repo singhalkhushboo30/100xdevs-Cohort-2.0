143a03ebec1c52ea990c984564b61945162bd2bc
Khushboo <khushboosinghal9760@gmail.com>
2024-01-09 21:24:39 +0530

    done

--- 0-1_Cohort/Week-5/Class-Content/React/currencyConverter/src/hooks/useCurrencyInfo.js
+++ 0-1_Cohort/Week-5/Class-Content/React/currencyConverter/src/hooks/useCurrencyInfo.js
@@ -2,7 +2,7 @@ import {useEffect,useState} from 'raect'
 
 function useCurrencyInfo(currency){
    useEffect(()=>{
-    fetch()
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`).then((res)=>{res.json()}).then((res)=>)
    },[])
 
 }
